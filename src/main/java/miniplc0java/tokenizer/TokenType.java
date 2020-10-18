771ba8f079b2acef69f93559d81eca9cf47be34e
HolmiumTS <42644820+HolmiumTS@users.noreply.github.com>
2020-10-18 12:41:21 +0800

    Update TokenType.java

--- src/main/java/miniplc0java/tokenizer/TokenType.java
+++ src/main/java/miniplc0java/tokenizer/TokenType.java
@@ -1,80 +1,83 @@
 package miniplc0java.tokenizer;
 
 public enum TokenType {
-    /** 空 */
-    None,
-    /** 无符号整数 */
-    Uint,
-    /** 标识符 */
-    Ident,
-    /** Begin */
-    Begin,
-    /** End */
-    End,
-    /** Var */
-    Var,
-    /** Const */
-    Const,
-    /** Print */
-    Print,
-    /** 加号 */
-    Plus,
-    /** 减号 */
-    Minus,
-    /** 乘号 */
-    Mult,
-    /** 除号 */
-    Div,
-    /** 等号 */
-    Equal,
-    /** 分号 */
-    Semicolon,
-    /** 左括号 */
-    LParen,
-    /** 右括号 */
-    RParen,
-    /** 文件尾 */
-    EOF;
+    /**
+     * 空
+     */
+    None("NullToken"),
+    /**
+     * 无符号整数
+     */
+    Uint("UnsignedInteger"),
+    /**
+     * 标识符
+     */
+    Ident("Identifier"),
+    /**
+     * Begin
+     */
+    Begin("Begin"),
+    /**
+     * End
+     */
+    End("End"),
+    /**
+     * Var
+     */
+    Var("Var"),
+    /**
+     * Const
+     */
+    Const("Const"),
+    /**
+     * Print
+     */
+    Print("Print"),
+    /**
+     * 加号
+     */
+    Plus("PlusSign"),
+    /**
+     * 减号
+     */
+    Minus("MinusSign"),
+    /**
+     * 乘号
+     */
+    Mult("MultiplicationSign"),
+    /**
+     * 除号
+     */
+    Div("DivisionSign"),
+    /**
+     * 等号
+     */
+    Equal("EqualSign"),
+    /**
+     * 分号
+     */
+    Semicolon("Semicolon"),
+    /**
+     * 左括号
+     */
+    LParen("LeftBracket"),
+    /**
+     * 右括号
+     */
+    RParen("RightBracket"),
+    /**
+     * 文件尾
+     */
+    EOF("EOF");
+
+    private TokenType(String name) {
+        this.name = name;
+    }
+
+    private final String name;
 
     @Override
     public String toString() {
-        switch (this) {
-            case None:
-                return "NullToken";
-            case Begin:
-                return "Begin";
-            case Const:
-                return "Const";
-            case Div:
-                return "DivisionSign";
-            case EOF:
-                return "EOF";
-            case End:
-                return "End";
-            case Equal:
-                return "EqualSign";
-            case Ident:
-                return "Identifier";
-            case LParen:
-                return "LeftBracket";
-            case Minus:
-                return "MinusSign";
-            case Mult:
-                return "MultiplicationSign";
-            case Plus:
-                return "PlusSign";
-            case Print:
-                return "Print";
-            case RParen:
-                return "RightBracket";
-            case Semicolon:
-                return "Semicolon";
-            case Uint:
-                return "UnsignedInteger";
-            case Var:
-                return "Var";
-            default:
-                return "InvalidToken";
-        }
+        return this.name;
     }
 }
